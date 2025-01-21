10bb13aab49e634b0f12ad22a25eff33f2ba3a72
sav04eck <6124466@gmail.com>
2025-01-21 19:18:22 +0300

    главная игровая функция + класс облаков, классы препятствий

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ pygame.init()
 WINDOW_HEIGHT = 600
 WINDOW_WIDTH = 1100
 WINDOW = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption("SonicRun")
 
 SONIC_RUNNING = [
     pygame.image.load(os.path.join("Texture/Sonic", "SonicRun1.png")),
@@ -113,3 +114,132 @@ class SonicCharacter:
 
     def draw(self, WINDOW):
         WINDOW.blit(self.current_image, (self.character_rect.x, self.character_rect.y))
+
+class SkyCloud:
+    def __init__(self):
+        self.x_position = WINDOW_WIDTH + random.randint(800, 1000)
+        self.y_position = random.randint(50, 100)
+        self.cloud_image = SKY_CLOUD
+        self.cloud_width = self.cloud_image.get_width()
+
+    def update(self):
+        self.x_position -= game_speed
+        if self.x_position < -self.cloud_width:
+            self.x_position = WINDOW_WIDTH + random.randint(2500, 3000)
+            self.y_position = random.randint(50, 100)
+
+    def draw(self, WINDOW):
+        WINDOW.blit(self.cloud_image, (self.x_position, self.y_position))
+
+class Obstacle:
+    def __init__(self, images, obstacle_type):
+        self.images = images
+        self.obstacle_type = obstacle_type
+        self.obstacle_rect = self.images[self.obstacle_type].get_rect()
+        self.obstacle_rect.x = WINDOW_WIDTH
+
+    def update(self):
+        self.obstacle_rect.x -= game_speed
+        if self.obstacle_rect.x < -self.obstacle_rect.width:
+            obstacle_list.pop()
+
+    def draw(self, WINDOW):
+        WINDOW.blit(self.images[self.obstacle_type], self.obstacle_rect)
+
+class SmallObstacle(Obstacle):
+    def __init__(self, images):
+        self.obstacle_type = random.randint(0, 2)
+        super().__init__(images, self.obstacle_type)
+        self.obstacle_rect.y = 325
+
+class LargeObstacle(Obstacle):
+    def __init__(self, images):
+        self.obstacle_type = random.randint(0, 2)
+        super().__init__(images, self.obstacle_type)
+        self.obstacle_rect.y = 300
+
+class FlyingDragon(Obstacle):
+    def __init__(self, images):
+        self.obstacle_type = 0
+        super().__init__(images, self.obstacle_type)
+        self.obstacle_rect.y = 250
+        self.animation_index = 0
+
+    def draw(self, WINDOW):
+        if self.animation_index >= 9:
+            self.animation_index = 0
+        WINDOW.blit(self.images[self.animation_index // 5], self.obstacle_rect)
+        self.animation_index += 1
+
+def main_game():
+    global game_speed, background_x_position, background_y_position, current_score, obstacle_list
+    is_running = True
+    game_clock = pygame.time.Clock()
+    player_character = SonicCharacter()
+    background_cloud = SkyCloud()
+    game_speed = 20
+    background_x_position = 0
+    background_y_position = 380
+    current_score = 0
+    font_style = pygame.font.Font('freesansbold.ttf', 20)
+    obstacle_list = []
+    death_count = 0
+
+    def update_score():
+        global current_score, game_speed
+        current_score += 1
+        if current_score % 100 == 0:
+            game_speed += 1
+
+        score_text = font_style.render("Очки: " + str(current_score), True, (0, 0, 0))
+        score_rect = score_text.get_rect()
+        score_rect.center = (100, 40)
+        WINDOW.blit(score_text, score_rect)
+
+    def draw_background():
+        global background_x_position, background_y_position
+        background_width = GAME_BACKGROUND.get_width()
+        WINDOW.blit(GAME_BACKGROUND, (background_x_position, background_y_position))
+        WINDOW.blit(GAME_BACKGROUND, (background_width + background_x_position, background_y_position))
+        if background_x_position <= -background_width:
+            background_x_position = 0
+        background_x_position -= game_speed
+
+    while is_running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                is_running = False
+
+        WINDOW.fill((0, 137, 255))
+        player_input = pygame.key.get_pressed()
+
+        player_character.draw(WINDOW)
+        player_character.update(player_input)
+
+        if len(obstacle_list) == 0:
+            random_choice = random.randint(0, 2)
+            if random_choice == 0:
+                obstacle_list.append(SmallObstacle(SMALL_OBSTACLES))
+            elif random_choice == 1:
+                obstacle_list.append(LargeObstacle(LARGE_OBSTACLES))
+            elif random_choice == 2:
+                obstacle_list.append(FlyingDragon(FLYING_DRAGON))
+
+        for obstacle in obstacle_list:
+            obstacle.draw(WINDOW)
+            obstacle.update()
+            if player_character.character_rect.colliderect(obstacle.obstacle_rect):
+                pygame.time.delay(2000)
+                death_count += 1
+                save_score_to_database(current_score)
+                main_menu(death_count)
+
+        draw_background()
+
+        background_cloud.draw(WINDOW)
+        background_cloud.update()
+
+        update_score()
+
+        game_clock.tick(30)
+        pygame.display.update()
